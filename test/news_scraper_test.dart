e461c4e89339cf617c3001f3abacfcb0f62f39ef
Rizentium Kingdom <rizentium@Rizentiums-MacBook-Pro.local>
2020-07-21 11:01:21 +0700

    Added goriau unit test

--- test/news_scraper_test.dart
+++ test/news_scraper_test.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:news_scraper/interfaces/resource.dart';
 import 'package:news_scraper/news_scraper.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final newsScraper = NewsScraper();
-    var result = newsScraper.getListFromSource(source: 'goriau');
-    result.then((value) {
-      print(value);
+  test('goriau.com', () async {
+    var newsScraper = NewsScraper();
+
+    var goriau = await newsScraper.getListFromSource(source: NewsSource.goriau);
+    // check the data length
+    expect(goriau.length, 19);
+
+    goriau.forEach((element) {
+      // is the title still valid
+      expect(element.title.length, greaterThanOrEqualTo(5));
+      // is the thumbnail still valid
+      expect(Uri.parse(element.thumbnail).isAbsolute, true);
+      // is the url still valid
+      expect(Uri.parse(element.url).isAbsolute, true);
+      // is the publishedAt still valid
+      expect(element.publishedAt,
+          DateTime.parse(element.publishedAt.toIso8601String()));
     });
   });
 }
